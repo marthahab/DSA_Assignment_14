0772feee29234933a22d0fcd0c9c9a32f344ddf6
marthahab <marthahabtamu264@gmail.com>
2026-01-31 11:07:53 -0800

    memory block

--- Problem_28_Memory_Manager/src/MemoryBlock.java
+++ Problem_28_Memory_Manager/src/MemoryBlock.java
@@ -1,5 +1,26 @@
 package Problem_28_Memory_Manager.src;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemoryBlock {
-    
+
+        boolean allocated;
+
+        boolean visited;
+
+        List<Integer> references;
+
+        public MemoryBlock() {
+            this.allocated = false;
+            this.visited = false;
+            this.references = new ArrayList<>();
+        }
+
+
+        public void resetVisited() {
+            this.visited = false;
+        }
+    }
+
+
 }
